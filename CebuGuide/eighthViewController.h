b41e9c1d0411399db34ae8799d96e0494055c53b
市川 貴章 <icchy.2323@gmail.com>
2015-02-09 14:44:15 +0900

    追加

--- CebuGuide/eighthViewController.h
+++ CebuGuide/eighthViewController.h
@@ -6,13 +6,15 @@
 //  Copyright (c) 2015年 市川 貴章. All rights reserved.
 //
 
-#import "ViewController.h"
+#import <UIKit/UIKit.h>
 
-@interface eighthViewController : ViewController
+@interface eighthViewController : UIViewController
 @property (weak, nonatomic) IBOutlet UIImageView *myImageView;
 @property (weak, nonatomic) IBOutlet UILabel *myLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *myImageView2;
 @property (weak, nonatomic) IBOutlet UILabel *myLabel2;
 @property (weak, nonatomic) IBOutlet UITextView *myTextView;
+- (IBAction)tapBtn:(id)sender;
+- (IBAction)tapBtn2:(id)sender;
 
 @end
--- CebuGuide/eighthViewController.m
+++ CebuGuide/eighthViewController.m
@@ -34,4 +34,27 @@
 }
 */
 
+- (IBAction)tapBtn:(id)sender {
+    
+    //    AlertViewの設定
+    UIAlertView *alert= [[UIAlertView alloc]
+                         initWithTitle:@"Bookmark" message:@"に追加しますか?" delegate:self cancelButtonTitle:@"cancel" otherButtonTitles:@"OK", nil];
+    //    設定したAlertViewを表示
+    [alert show];
+}
+
+- (IBAction)tapBtn2:(id)sender {
+}
+
+//ボタンがクリックされた時に、どのボタンが押されたか認識できるメソッド
+- (void)alertView:(UIAlertView *)alertView
+clickedButtonAtIndex:(NSInteger)buttonIndex{
+    
+    if (buttonIndex == 1) {
+        NSLog(@"OK");
+    } else {
+        NSLog(@"Cancel");
+    }
+}
+
 @end
